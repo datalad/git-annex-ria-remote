aa4d329d8f89d26914054f74487e498708ac5ceb
Michael Hanke <michael.hanke@gmail.com>
2019-08-19 20:45:06 +0200

    Towards an @effigies inspired packaging setup

--- setup.py
+++ setup.py
@@ -1,37 +1,18 @@
 #!/usr/bin/env python
+import sys
+from setuptools import setup
+import versioneer
 
-import os.path as op
-from setuptools import (
-    setup,
-    find_packages,
-)
+# Give setuptools a hint to complain if it's too old a version
+# 30.3.0 allows us to put most metadata in setup.cfg
+# Should match pyproject.toml
+SETUP_REQUIRES = ['setuptools >= 30.3.0']
+# This enables setuptools to install wheel on-the-fly
+SETUP_REQUIRES += ['wheel'] if 'bdist_wheel' in sys.argv else []
 
-
-def get_version():
-    """Load version of datalad from version.py without entailing any imports
-    """
-    # This might entail lots of imports which might not yet be available
-    # so let's do ad-hoc parsing of the version.py
-    with open(op.join(op.dirname(__file__), 'ria_remote', 'version.py')) as f:
-        version_lines = list(filter(lambda x: x.startswith('__version__'), f))
-    assert (len(version_lines) == 1)
-    return version_lines[0].split('=')[1].strip(" '\"\t\n")
-
-
-setup(
-    name="ria_remote",
-    author="Michael Hanke",
-    author_email="michael.hanke@gmail.com",
-    version=get_version(),
-    description="Git-annex special remote implementation for (remote) indexed archives",
-    long_description="""""",
-    install_requires=[
-        'datalad>=0.12.0rc4',
-        'annexremote',
-        'future',
-    ],
-    packages=find_packages(),
-    scripts=[
-        op.join('bin', 'git-annex-remote-ria'),
-    ],
-)
+if __name__ == '__main__':
+    setup(name='ria_remote',
+          version=versioneer.get_version(),
+          cmdclass=versioneer.get_cmdclass(),
+          setup_requires=SETUP_REQUIRES,
+          )

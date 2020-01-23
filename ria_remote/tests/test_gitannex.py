80c1b027ff4bc864bae43c4f506e84e8edad3f5e
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2020-01-23 16:26:34 +0100

    TST: Remove deprecated config-based annex-testremote runs

--- ria_remote/tests/test_gitannex.py
+++ ria_remote/tests/test_gitannex.py
@@ -13,21 +13,6 @@ from ria_remote.tests.utils import (
 @skip_non_ssh  # superfluous in an SSH-run and annex-testremote is slow
 @with_tempfile(mkdir=True)
 @with_tempfile()
-def test_gitannex_localio(path, objtree):
-    ds = create(path)
-    initexternalremote(
-        ds.repo, 'ria-local', 'ria',
-        config={'base-path': objtree})
-    ds.repo._run_annex_command(
-        'testremote',
-        annex_options=['ria-local'],
-        log_stdout=False,
-    )
-
-
-@skip_non_ssh  # superfluous in an SSH-run and annex-testremote is slow
-@with_tempfile(mkdir=True)
-@with_tempfile()
 def test_gitannex_localio_url(path, objtree):
     ds = create(path)
     initexternalremote(
@@ -36,24 +21,6 @@ def test_gitannex_localio_url(path, objtree):
     ds.repo._run_annex_command(
         'testremote',
         annex_options=['ria-local'],
-        log_stdout=False,
-    )
-
-
-@skip_ssh
-@with_tempfile(mkdir=True)
-@with_tempfile()
-def test_gitannex_remoteio(path, objtree):
-    ds = create(path)
-    initexternalremote(
-        ds.repo, 'ria-remote', 'ria',
-        config={
-            'base-path': objtree,
-            'ssh-host': 'datalad-test',
-        })
-    ds.repo._run_annex_command(
-        'testremote',
-        annex_options=['ria-remote'],
         log_stdout=False,
     )
 
